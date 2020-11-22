1c2828e9ab2f75aa583ccdbdfa4820b60132cf58
shabnam <shabnam.mostary@gmail.com>
2020-11-22 00:04:32 -0500

    created pug file for index & students page

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
-
+const { departments, students, faculty } = require('./schools')
+// const bodyParser = require('body-parser')
 const app = express()
+
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  return response.render('index')
+})
+app.get('/students', (request, response) => {
+  return response.render('students', { students })
+})
 
 
 
